f551db3ccecee7de54f80d2a8bacb1c1015ee14a
Михаил <misha1234567890m@gmail.com>
2022-08-10 14:57:10 +0300

    рефакторинг

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,67 +1,65 @@
-import React from 'react'
+import React from "react";
 
-
-
-
-const Drawer = ({onClickKrest,items=[],onRemove}) => {
-
-  
-
+const Drawer = ({ onClickKrest, items = [], onRemove }) => {
   return (
-    <div  className="overlay">
-    <div className="drawer">
-    <div>
-      <h2>
-        Корзина
-        <img className="n" height={20} src="img/krest.png" alt="" onClick={onClickKrest}/>
-      </h2>
-    </div>
-    
-      <div className="Item">
-        {items.map((items) =>(
-          
-          <div className="cartItem">
-          <div
-            style={{ backgroundImage: `url(${items.imageUrl}` }}
-            className="cartItemImg"
-          ></div>
-
-          <div className={{height: "1000px"}}>
-            <p>{items.title } </p>
-            <b style={{padding : "6px"}} >{items.price} </b>
-          </div>
-         <img className="m" height={20} src="img/krest.png" alt="Krest" onClick={()=>onRemove(items.id)} />
+    <div className="overlay">
+      <div className="drawer">
+        <div>
+          <h2>
+            Корзина
+            <img
+              className="n"
+              height={20}
+              src="img/krest.png"
+              alt=""
+              onClick={onClickKrest}
+            />
+          </h2>
         </div>
-        )
-          
-        
-          
-        )}
-        
+
+        <div className="Item">
+          {items.map((items) => (
+            <div className="cartItem">
+              <div
+                style={{ backgroundImage: `url(${items.imageUrl}` }}
+                className="cartItemImg"
+              ></div>
+
+              <div className={{ height: "1000px" }}>
+                <p>{items.title} </p>
+                <b style={{ padding: "6px" }}>{items.price} </b>
+              </div>
+              <img
+                className="m"
+                height={20}
+                src="img/krest.png"
+                alt="Krest"
+                onClick={() => onRemove(items.id)}
+              />
+            </div>
+          ))}
+        </div>
+        <div className="Korz">
+          <ul>
+            <li className="F">
+              <span>Итого:</span>
+              <div></div>
+              <b>700руб</b>
+            </li>
+            <li>
+              <span>Доставка</span>
+              <div></div>
+              <b>200 руб</b>
+            </li>
+          </ul>
+
+          <button className="btn">
+            Оформить заказ <img width={15} src="img/strelka.png" alt="" />
+          </button>
+        </div>
       </div>
-      <div className="Korz">
-        <ul>
-          <li className="F">
-            <span>Итого:</span>
-            <div></div>
-            <b>700руб</b>
-          </li>
-          <li>
-            <span>Доставка</span>
-            <div></div>
-            <b>200 руб</b>
-          </li>
-        </ul>
+    </div>
+  );
+};
 
-        <button className="btn">
-          Оформить заказ <img width={15} src="img/strelka.png" alt="" />
-        </button>
-      </div>
-    
-  </div>
-  </div>
-
-  )
-}
-
-export default Drawer
+export default Drawer;
